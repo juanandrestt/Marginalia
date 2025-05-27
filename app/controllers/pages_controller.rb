155cef1f8535f8652c45b553186b150fbc4d4525
anthonymicho <anthony.micho15@gmail.com>
2025-05-28 01:33:14 +0200

    Text ux ui

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @book = Book.limit(1)
+     @books = Book.offset(1).limit(3)
+  end
+  def books
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    if params[:query].present?
+      @books = Book.where("title ILIKE ? OR author ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @books = Book.all
+    end
   end
 end
