362b10c5d70873e3335c2aa0ac10d5ffe0209d06
anthonymicho <anthony.micho15@gmail.com>
2025-05-27 17:27:19 +0200

    Review works

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @current_user_review = @book.reviews.where(user: current_user).first
     @reviews = @book.reviews.where.not(user: current_user)
-    @current_user_review = @book.reviews.where(user: current_user)
   end
 
   def new
