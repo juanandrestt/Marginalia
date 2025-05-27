27f8ec232dfa95c556623c595afc454c1275fc26
Juan Trujillo <juan.trujillo1@protonmail.com>
2025-05-27 16:24:16 +0200

    Update user devise

--- db/schema.rb
+++ db/schema.rb
@@ -75,10 +75,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_125531) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
