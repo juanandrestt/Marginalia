9a5cdcc731f560a4b0fc3222464d267cb570db85
Samuel GOLDERY <samuel.goldery@gmail.com>
2025-05-27 15:38:00 +0200

    modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  devise_for :users
+  
   resources :lists, only: %i[ new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
+
   resources :books, only: %i[index show]
 end
