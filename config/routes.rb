90c673b3bc74caed2aac469e981f22b7d1fa4f53
Juan Trujillo <juan.trujillo1@protonmail.com>
2025-05-27 15:25:58 +0200

    Create an AI assistant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  resources :messages, only: [:index, :new, :create]
   resources :lists, only: %i[ new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
