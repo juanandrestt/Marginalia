b5cd6eebf46dd40b13756e72d3b68e30c4eeec35
Samuel GOLDERY <samuel.goldery@gmail.com>
2025-05-27 15:57:53 +0200

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_100827) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_125531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,10 +27,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_100827) do
     t.string "title"
     t.string "author"
     t.integer "publishing_year"
-    t.string "genre"
-    t.string "characters"
+    t.text "characters"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "open_library_id"
+    t.text "description"
+    t.string "cover_url"
+    t.text "subjects"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -39,6 +42,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_100827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "readings", force: :cascade do |t|
