070666d6bdf777a10441f0aa04f72caf2759fa30
anthonymicho <anthony.micho15@gmail.com>
2025-05-27 12:47:56 +0200

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,55 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
 require "json"
 require "open-uri"
 
-url = "https://openlibrary.org/search.json?q=*&limit=100&page=1"
-books_serialized = URI.parse(url).read
-books = JSON.parse(book_serialized)
-
-books.each do |books|
+subject = "fantasy"  # Change this to your desired subject
+url = "https://openlibrary.org/subjects/#{subject}.json?limit=50"
+serialized = URI.open(url).read
+data = JSON.parse(serialized)
+data["works"].each do |work|
+  title = work["title"]
+  authors = work["authors"] || []
+  author = authors.first ? authors.first["name"] : "Unknown Author"
+  publishing_year = work["first_publish_year"]
+  open_library_id = work["key"]&.split("/")&.last
+  subjects = work["subject"]&.join(", ") || ""
+  description = work["description"]
+  # description can be string or hash with 'value'
+  if description.is_a?(Hash)
+    description = description["value"]
+  elsif !description.is_a?(String)
+    description = nil
+  end
+  cover_id = work["cover_id"]
+  cover_url = cover_id ? "https://covers.openlibrary.org/b/id/#{cover_id}-L.jpg" : nil
+  characters = nil # not available here, could add later
   Book.create!(
-    title: ["title"],
-    overview: movie["overview"],
-    poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
-    rating: movie["vote_average"]
+    title: title,
+    author: author,
+    publishing_year: publishing_year,
+    open_library_id: open_library_id,
+    description: description,
+    cover_url: cover_url,
+    subjects: subjects,
+    characters: characters
   )
 end
+
+
+# require "json"
+# require "open-uri"
+
+# url = "https://openlibrary.org/search.json?q=*&limit=100&page=1"
+# books_serialized = URI.parse(url).read
+# books = JSON.parse(book_serialized)
+
+# books.each do |books|
+#   Book.create!(
+#     title: ["title"],
+#     overview: movie["overview"],
+#     poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
+#     rating: movie["vote_average"]
+#   )
+# end
