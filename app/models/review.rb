81ba0ebaf4840cf18a6e4334813d231e08d999dd
anthonymicho <anthony.micho15@gmail.com>
2025-06-02 16:09:46 +0200

    Add comments

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   # PENSER A METTRE UNE VALIDATION POUR UNE REVIEW PAR LIVRE PAR USER
 end
