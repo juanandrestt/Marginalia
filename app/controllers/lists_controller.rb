87c5ecf2c9679ed0a95163464fb3c659d63cb0b1
anthonymicho <anthony.micho15@gmail.com>
2025-06-06 12:40:46 +0200

    Realy good fix

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,22 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :destroy]
-  
+
   def new
     @list = List.new
+    @book = Book.find(params[:book_id]) if params[:book_id]
   end
 
   def create
     @list = List.new(list_params)
     @list.user = current_user
+
+    if params[:list][:book_id]
+      book = Book.find(params[:list][:book_id])
+      @list.books << book
+    end
+
     if @list.save
-      redirect_to dashboard_path
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
