f3b17e60be4b188996cfa7188f3e5a9af6d3c71c
anthonymicho <anthony.micho15@gmail.com>
2025-06-10 22:29:14 +0200

    Home update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
   def home
-    if user_signed_in?
     @book = Book.limit(1)
     @books = Book.offset(20).limit(6)
     @booksreco = Book.offset(30).limit(6)
     @last = Book.offset(10).limit(6)
-        else
-      redirect_to new_user_session_path
-    end
+    @bookslider = Book.limit(6)
   end
 
   def books
@@ -24,8 +21,8 @@ class PagesController < ApplicationController
 
   def dashboard
     if user_signed_in?
-    @recent_lists = List.where(user_id: current_user.id).order(created_at: :desc).limit(5)
-    @recent_readings = Reading.where(user_id: current_user.id).order(created_at: :desc).limit(5)
+      @recent_lists = List.where(user_id: current_user.id).order(created_at: :desc).limit(5)
+      @recent_readings = Reading.where(user_id: current_user.id).order(created_at: :desc).limit(5)
     else
       redirect_to new_user_session_path
     end
